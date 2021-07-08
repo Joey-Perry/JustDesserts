447d5f0b7311eb816ea51a024df348c4d7d4c9df
Joey-Perry <Joeyap84@gmail.com>
2021-07-08 07:22:09 -0500

    updated menu styling

--- menuItems.js
+++ menuItems.js
@@ -39,10 +39,22 @@ function createMenuItems(i, imgUrl, title, price, description){
 const oreoDescription = 'A fan favorite! Cookies and cream cheese combined to make a melt in your mouth' +
 ' treat covered with an outer layer of hardened chocolate.';
 
-createMenuItems(0, "https://res.cloudinary.com/hannahj/image/upload/v1576307862/IMG_3963_2_tmuk7v.jpg", 'Oreo Truffles |', '$15', oreoDescription)
-createMenuItems(1, "https://res.cloudinary.com/hannahj/image/upload/v1591142585/babyshowercakeballs_hwcgat.jpg", 'Cake Balls |', '$15', oreoDescription)
-createMenuItems(2, "https://res.cloudinary.com/hannahj/image/upload/v1576306736/IMG_3600_3_hrpdnx.jpg", 'The Goodness |', '$20', oreoDescription)
-createMenuItems(3, "https://res.cloudinary.com/hannahj/image/upload/v1576306990/IMG_3832_t5ndnk.jpg", 'Chocolate Pie |', '$15', oreoDescription)
-createMenuItems(4, "https://res.cloudinary.com/hannahj/image/upload/v1576307549/IMG_3790_avonru.jpg", 'Cheesecake Brownies |', '$25', oreoDescription)
-createMenuItems(5, "https://res.cloudinary.com/hannahj/image/upload/v1591143453/mintchocolatelasagna_fcvwdc.jpg", 'Mint Chocolate Lasagna |', '$25', oreoDescription)
-createMenuItems(6, "https://res.cloudinary.com/hannahj/image/upload/v1576307224/image000000_zaccck.jpg", 'Chocolate Chip Cookies |', '$10', oreoDescription)
+const cakeBallDescription = 'Popular with kids and adults alike! Everyone enjoys these sweet treats that are soft on the inside and candy coated on the outside. Perfect for any occassion!';
+const goodnessDesc = 'A creamy mix of whipped cream, peanut butter and crushed oreos. If you want to be the hit of your next party, bring this delicious and unique dish!';
+const chocolatePieDesc = 'Looking for a chilled dessert that will scratch that chocolate itch? This chocolate pie is simple and yet a complex mix of quality flavors and exquisite presentation.';
+const cheesecakeBrowniesDesc = "An up and coming item that is sure to become a classic! An amazing mix of cream cheese and brownies, this is something you'll order two of after you've tried it!";
+const mintChocDesc = 'A frozen paradise of chocolate and mint! With the consistency of ice-cream cake, this dessert layered with chocolate puddding, cream cheese, cool whip is definitely what you need to cool down on a hot summer day!';
+const chocolateChipDesc = "The classic of all classics! Two dozen of these will change your life for the better! If you're a fan of chocolate but you're not sure what to get, this is a great starting point.";
+
+createMenuItems(0, "https://res.cloudinary.com/hannahj/image/upload/v1576307862/IMG_3963_2_tmuk7v.jpg", 'Oreo Truffles |', '$15', oreoDescription);
+createMenuItems(1, "https://res.cloudinary.com/hannahj/image/upload/v1591142585/babyshowercakeballs_hwcgat.jpg", 'Cake Balls |', '$15', cakeBallDescription);
+createMenuItems(2, "https://res.cloudinary.com/hannahj/image/upload/v1576306736/IMG_3600_3_hrpdnx.jpg", 'The Goodness |', '$20', goodnessDesc);
+createMenuItems(3, "https://res.cloudinary.com/hannahj/image/upload/v1576306990/IMG_3832_t5ndnk.jpg", 'Chocolate Pie |', '$15', chocolatePieDesc);
+createMenuItems(4, "https://res.cloudinary.com/hannahj/image/upload/v1576307549/IMG_3790_avonru.jpg", 'Cheesecake Brownies |', '$25', cheesecakeBrowniesDesc);
+createMenuItems(5, "https://res.cloudinary.com/hannahj/image/upload/v1591143453/mintchocolatelasagna_fcvwdc.jpg", 'Mint Chocolate Lasagna |', '$25', mintChocDesc);
+createMenuItems(6, "https://res.cloudinary.com/hannahj/image/upload/v1576307224/image000000_zaccck.jpg", 'Chocolate Chip Cookies |', '$10', chocolateChipDesc);
+createMenuItems(7, "https://res.cloudinary.com/hannahj/image/upload/v1576307224/image000000_zaccck.jpg", 'Chocolate Chip Cookies |', '$10', chocolateChipDesc);
+createMenuItems(8, "https://res.cloudinary.com/hannahj/image/upload/v1576307224/image000000_zaccck.jpg", 'Chocolate Chip Cookies |', '$10', chocolateChipDesc);
+createMenuItems(9, "https://res.cloudinary.com/hannahj/image/upload/v1576307224/image000000_zaccck.jpg", 'Chocolate Chip Cookies |', '$10', chocolateChipDesc);
+createMenuItems(10, "https://res.cloudinary.com/hannahj/image/upload/v1576307224/image000000_zaccck.jpg", 'Chocolate Chip Cookies |', '$10', chocolateChipDesc);
+createMenuItems(11, "https://res.cloudinary.com/hannahj/image/upload/v1576307224/image000000_zaccck.jpg", 'Chocolate Chip Cookies |', '$10', chocolateChipDesc);
