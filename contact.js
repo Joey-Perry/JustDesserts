b5990e464b3f3e12b2609dd701cbd427541009fb
Joey-Perry <Joeyap84@gmail.com>
2021-12-22 09:59:10 -0600

    Added contact page

---
+++ contact.js
@@ -0,0 +1,8 @@
+const grab = (element) => document.querySelector(element);
+
+const submitBtn = grab('#contact-submit-btn');
+
+// submitBtn.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     console.log('Click!')
+// })
